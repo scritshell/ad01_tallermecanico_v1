546b2f2bcf94f7ebb69acb443226d5b5f987034f
scritshell <sebastiancsibi2@gmail.com>
2025-12-07 10:42:40 +0100

    Creacion del paquete ficheros.json Creacion de las clases Clientes, Trabajos, Vehiculos y FuenteDatosFicherosJSON implementando de IFuenteDatos.

---
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/negocio/ficheros/json/FuenteDatosFicherosJSON.java
@@ -0,0 +1,21 @@
+package org.iesalandalus.programacion.tallermecanico.modelo.negocio.ficheros.json;
+
+import org.iesalandalus.programacion.tallermecanico.modelo.negocio.IClientes;
+import org.iesalandalus.programacion.tallermecanico.modelo.negocio.IFuenteDatos;
+import org.iesalandalus.programacion.tallermecanico.modelo.negocio.ITrabajos;
+import org.iesalandalus.programacion.tallermecanico.modelo.negocio.IVehiculos;
+
+public class FuenteDatosFicherosJSON implements IFuenteDatos {
+    @Override
+    public IClientes crearClientes() {
+        return Clientes.getInstancia();
+    }
+    @Override
+    public IVehiculos crearVehiculos() {
+        return Vehiculos.getInstancia();
+    }
+    @Override
+    public ITrabajos crearTrabajos() {
+        return Trabajos.getInstancia();
+    }
+}
---
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/negocio/ficheros/json/Vehiculos.java
@@ -0,0 +1,4 @@
+package org.iesalandalus.programacion.tallermecanico.modelo.negocio.ficheros.json;
+
+public class Vehiculos {
+}
