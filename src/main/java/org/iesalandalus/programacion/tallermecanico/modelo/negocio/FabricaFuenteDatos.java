28a7b4288723d1c00ca45baff11d3209f923d1bf
scritshell <sebastiancsibi2@gmail.com>
2025-12-07 16:46:50 +0100

    Añadido FICHEROS_JSON

--- src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/negocio/FabricaFuenteDatos.java
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/negocio/FabricaFuenteDatos.java
@@ -1,14 +1,22 @@
 package org.iesalandalus.programacion.tallermecanico.modelo.negocio;
 
 import org.iesalandalus.programacion.tallermecanico.modelo.negocio.ficheros.xml.FuenteDatosFicherosXML;
-
+import org.iesalandalus.programacion.tallermecanico.modelo.negocio.ficheros.json.FuenteDatosFicherosJSON;
 
 public enum FabricaFuenteDatos {
-
     FICHEROS_XML {
         @Override
         public IFuenteDatos crear() {
             return new FuenteDatosFicherosXML();
+        }
+    },
+
+
+    // Opcion de ficheros formato JSON
+    FICHEROS_JSON {
+        @Override
+        public IFuenteDatos crear() {
+            return new FuenteDatosFicherosJSON();
         }
     };
 
